8edc37f063a7179e4b1c5a9c963c994ebf96fa57
tugcetorunn <tokerrtugcee@gmail.com>
2024-07-12 13:47:49 +0300

    MVC arguments added.

--- MultiShop.Presentation/Program.cs
+++ MultiShop.Presentation/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // controller ve view leri görebilmesi için
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting(); // route u eklemeden önce yazýyoruz
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}"); // route olmazsa olmaz
 
 app.Run();
+
+// projemizin ayaða kalktýðý yer. 
